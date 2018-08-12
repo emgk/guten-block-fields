d2fd6c7a5d40c10fb643350167d31cc949364e37
Govind Kumar <gkprmr@gmail.com>
2018-08-12 13:17:43 +0530

    command management code.

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 #! /usr/bin/env node
 
 const chalk = require('chalk');
+const program = require('commander');
+const package = require('./package.json');
 
-// Get the arguments.
-const [, , ...args] = process.argv
+// Set the command.
+program
+    .version('1.0.0')
+    .arguments('<cmd> [block]')
+    .action((cmd, block) => {
+        command = cmd;
+        blockname = block;
+    });
 
-console.log(chalk.green(`Welcome to Gutenberg Inspect Controller! ${args}`))
+// Parse the arguments.
+program.parse(process.argv);
+
+// if no command were provided.
+if (typeof command === "undefined") {
+    console.log(chalk.red(`No command were provided, please run '${package.name} --help' command`));
+    process.exit(1);
+}
+
+// run the script.
+switch (command) {
+    case 'generate':
+        console.log(chalk.green('We\'re all set to generate!'));
+        break;
+
+    // Default msg.
+    default:
+        console.log(chalk.red(`Invalid command, please run '${package.name} --help' command`));
+}
