4326a0448a96f62e34350a55b5747e663c16c7be
Govind Kumar <gkprmr@gmail.com>
2018-08-12 21:37:25 +0530

    handle text field file reading.

--- core/renderField.js
+++ core/renderField.js
@@ -1,19 +1,31 @@
 const path = require('path');
 const fs = require('fs');
+const chalk = require('chalk');
+const spinner = require('ora');
 
 const validate = require('./validates');
 
 module.exports.renderTextField = (field) => {
     let template = path.resolve(__dirname, '../gic-scripts/fields/text.tpl');
 
+    // Get the file content.
     fs.readFile(template, 'utf8', (err, contents) => {
-        let tempContents = contents.toString();
+
+        // If error.
+        if (err) {
+            console.log(
+                chalk.red(`Error while loading TextPlain template.`)
+            )
+            process.exit(1);
+        }
+
+        let textFieldContent = contents.toString();
 
         // Replace the content.
-        tempContents = tempContents.replace(`<%field-title%>`, `"${field.title}"`);
-        tempContents = tempContents.replace(`<%field-slug%>`, `${field.slug}`);
-        tempContents = tempContents.replace(`<%field-slug%>`, `${validate.makeComponentName(field.slug)}`);
+        textFieldContent = textFieldContent.replace(`<%field-title%>`, `"${field.title}"`);
+        textFieldContent = textFieldContent.replace(`<%field-slug%>`, `${field.slug}`);
+        textFieldContent = textFieldContent.replace(`<%field-slug%>`, `${validate.makeComponentName(field.slug)}`);
 
-        console.log(tempContents);
+        return textFieldContent;
     });
 }
