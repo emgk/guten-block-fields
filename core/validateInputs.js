4a6c372b230439a68d78ceef5989d0b06184df74
Govind Kumar <gkprmr@gmail.com>
2018-08-12 14:53:43 +0530

    add templates

---
+++ core/validateInputs.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+/**
+ * Validate and get the field name.
+ * 
+ * @since 1.0.0
+ * @param {String} name 
+ */
+module.exports.validateFieldName = (name) => {
+    return name.replace(/\s+/g, '-').toLowerCase()
+}
